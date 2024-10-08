8d2476847f2f498648d5dc57145abc9246c5fe89
Maarten Paulissen <maartenpaulissen99@gmail.com>
2024-10-08 21:34:13 +0200

    first commit

---
+++ 1-variabelen-operatoren/opdracht-1/opdracht-1.js
@@ -0,0 +1,4 @@
+let naam = "Jan";
+let leeftijd = 25;
+
+console.log(naam + " " + "is" + " " + leeftijd + " " + "jaar oud.");
---
+++ 1-variabelen-operatoren/opdracht-2/opdracht-2.js
@@ -0,0 +1,7 @@
+let a = 10;
+let b = 20;
+
+a = 20;
+b = 10;
+
+console.log(a, b);
